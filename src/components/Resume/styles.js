ced81f21f69a0ecedbd190e02c4082459193fbbe
lucascosta <lucas.costa@gnsystems.com.br>
2022-09-15 11:27:40 -0300

    inicio do projeto

---
+++ src/components/Resume/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-wdth: 1120px;
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  gap: 20px;
+  margin-top: -50px;
+  justify-content: space-around;
+`;
